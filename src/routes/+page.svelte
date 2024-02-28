801d0fca0b70f469987a301c8d535a6909d2edcb
Thierry Jones <jones.thierry@gmail.com>
2024-02-27 22:16:01 -0500

    more css changes and data tables

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -42,6 +42,7 @@
 	let future_days = 7; // Default number of future days to predict
 	let data = [];
 	let futureData = [];
+	let tableData =[];
   
 	async function loadModel() {
 	  try {
@@ -71,11 +72,14 @@
 		try {
 			loading = true;
 			// Fetch data
-			const stock_data = await getStockData(symbol, start_date, end_date);
+			let stock_data = await getStockData(symbol, start_date, end_date);
 
-			console.log("stock data directly from API:", stock_data['historical'])
+			console.log("stock data directly from API:", stock_data)
 
 			// Preprocess data
+			tableData = {...stock_data['historical'][0]};
+			delete tableData.label;
+
 			let { scaled_data, scaler, dates } = await preprocessData(stock_data['historical']);
 
 			console.log("scaled data after preprocess stage:", scaled_data)
@@ -112,7 +116,8 @@
 			const newData = [];
 			for (let idx = 0; idx < dates.length; idx++) {
 				const date = dates[idx];
-				const actual = y_actual[idx];
+				// const actual = y_actual[idx];
+				const actual = stock_data['historical'].map(price => price.close)[idx]
 				const predicted = y_pred_actual[idx];
 				if (actual !== undefined && predicted !== undefined) {
 					newData.push({ date, actualPrice: actual, predictedPrice: predicted.toFixed(2) });
@@ -185,11 +190,12 @@
 		// Normalize the data
 		const scaledData = closingPrices.map(price => [(price - minValue) / (maxValue - minValue)]);
 		const dates = data.map(day => day.date);
+		
 		return {
 			scaled_data: scaledData,
 			scaler: { minValue, maxValue },
-			dates: dates
-		};
+			dates: dates,
+		}
 	}
 
 	function createSequences(data, seq_length) {
@@ -254,27 +260,24 @@
 </script>
   
 <section>
-	<div class="sidebar-left">
+	<div class="sidebar-left" style="max-height: 75vh; overflow-y: auto;">
 		<div class="input-container">
 			<label for="symbol">Stock Symbol:</label>
 			<input type="text" id="symbol" bind:value={symbol} placeholder="Select a Stock/Enter your own">
 		</div>
 		<div class="input-container">
             <label for="selectedSector">Sector:</label>
-			<div class="card w-full py-4 px-1">
-				<div class="grid grid-cols-4 gap-4 justify-center">
-					<button class="btn {selectedSector === 'all' ? 'bg-gradient-to-br variant-gradient-primary-secondary' : 'variant-ghost-surface '}" on:click={() => selectedSector = 'all'}>All</button>
-					<button class="btn {selectedSector === 'tech' ? 'variant-filled-primary' : 'variant-ghost-surface '}" on:click={() => selectedSector = 'tech'}>Tech</button>
-					<button class="btn {selectedSector === 'food' ? 'variant-filled-primary' : 'variant-ghost-surface '}" on:click={() => selectedSector = 'food'}>Food</button>
-					<button class="btn {selectedSector === 'energy' ? 'variant-filled-primary' : 'variant-ghost-surface '}" on:click={() => selectedSector = 'energy'}>Energy</button>
-					<div class="col-span-4 gap-4 flex justify-center">
-						<button class="btn {selectedSector === 'utilities' ? 'variant-filled-primary' : 'variant-ghost-surface '}" on:click={() => selectedSector = 'utilities'}>Utilities</button>
-						<button class="btn {selectedSector === 'healthcare' ? 'variant-filled-primary' : 'variant-ghost-surface '}" on:click={() => selectedSector = 'healthcare'}>Healthcare</button>
-						<button class="btn {selectedSector === 'financials' ? 'variant-filled-primary' : 'variant-ghost-surface '}" on:click={() => selectedSector = 'financials'}>Financials</button>
-					</div>
+			<div class="card w-full py-2 px-0.5">
+				<div class="col-span-4 flex flex-wrap justify-center gap-4">
+					<button style="max-width: 5vw;" class="btn {selectedSector === 'all' ? 'bg-gradient-to-br variant-gradient-primary-secondary' : 'variant-ghost-surface '}" on:click={() => selectedSector = 'all'}>All</button>
+					<button style="max-width: 5vw;" class="btn {selectedSector === 'tech' ? 'variant-filled-primary' : 'variant-ghost-surface '}" on:click={() => selectedSector = 'tech'}>Tech</button>
+					<button style="max-width: 5vw;" class="btn {selectedSector === 'food' ? 'variant-filled-primary' : 'variant-ghost-surface '}" on:click={() => selectedSector = 'food'}>Food</button>
+					<button style="max-width: 5vw;" class="btn {selectedSector === 'energy' ? 'variant-filled-primary' : 'variant-ghost-surface '}" on:click={() => selectedSector = 'energy'}>Energy</button>
+					<button style="max-width: 5vw;" class="btn {selectedSector === 'utilities' ? 'variant-filled-primary' : 'variant-ghost-surface '}" on:click={() => selectedSector = 'utilities'}>Utilities</button>
+					<button style="max-width: 5vw;" class="btn {selectedSector === 'healthcare' ? 'variant-filled-primary' : 'variant-ghost-surface '}" on:click={() => selectedSector = 'healthcare'}>Health</button>
+					<button style="max-width: 5vw;" class="btn {selectedSector === 'financials' ? 'variant-filled-primary' : 'variant-ghost-surface '}" on:click={() => selectedSector = 'financials'}>Finance</button>
 				</div>
-			</div>
-						  
+			</div>		  
         </div>
 		<div class="input-container">
 			<label for="symbol">Select Stock:</label>
@@ -305,42 +308,61 @@
 			<input type="number" id="future_days" bind:value={future_days} min="1" max="14">
 		</div>
 		<div class="input-container">
-			<button class="btn btn-filled-primary" on:click={() => predict(symbol, start_date, end_date, future_days)}>Predict Stock Price</button>
+			<button class="btn btn-filled-primary" style="color: white;"on:click={() => predict(symbol, start_date, end_date, future_days)}>Predict Stock Price</button>
 		</div>
 	</div>
 	
 	
 	<div class="main-content">
-		<canvas id="myChart"></canvas>
-		{#if loading}
-        	<ProgressBar value={undefined} />
-    	{/if}
+		<div style="max-width: 45vw; overflow-x: auto;">
+			<canvas id="myChart"></canvas>
+			{#if loading}
+				<ProgressBar value={undefined} />
+			{/if}
+			{#if data.length > 1}
+			<div style="margin-top: 1vw; justify-content: center;">
+				<!-- <h2 class="h1 "style="margin-bottom: 1vw;">{symbol.toUpperCase()}</h2> -->
+				<Table
+					source={{
+						head: Object.keys(tableData).map(key => key.toString()),
+						body: [Object.values(tableData).map(value => value.toString())]
+					}}
+				/>
+			</div>
+			{/if}
+		</div>
 	</div>
 		
 	<div class="right-content">
 		<div style="max-height: 75vh; overflow-y: auto;">
-		<Table
-			source={
-				data.length > 1 ? // Check if data is not empty
-				{
-					head: ['Dates', '$ Actual', ' $ Predicted'],
-					body: [
-					
-					...futureData.map(({ date, predictedPrice }) => [date, 'N/A', predictedPrice]),
-					...data.map(({ date, actualPrice, predictedPrice }) => [date, actualPrice, predictedPrice])
-					],
-					foot: ['Total', '', data.length + futureData.length]
+			{#if data.length > 1}
+			<Table
+				source={
+					data.length > 1 ? // Check if data is not empty
+					{
+						head: ['Dates', '$ Actual', '$ Predicted'],
+						body: [
+							...futureData.map(({ date, predictedPrice }) => {
+								return [
+									date,
+									`<span>N/A</span>`,
+									`<span style="color: red; font-weight: bold;">${predictedPrice}</span>`
+								];
+							}),
+							...data.map(({ date, actualPrice, predictedPrice }) => [date, actualPrice, predictedPrice])
+						],
+						foot: ['Total', '', data.length + futureData.length]
+					}
+					:
+					{
+						head: ['Dates', '$ Actual', '$ Predicted'],
+						body: [[0, 0, 0]], // Display 0 values if data is empty
+						foot: ['Total', '', 0]
+					}
 				}
-				:
-				{
-					head: ['Dates', '$ Actual', '$ Predicted'],
-					body: [[0, 0, 0]], // Display 0 values if data is empty
-					foot: ['Total', '', 0]
-				}
-			}
-		/>
+			/>
+			{/if}
 		</div>
-
 	</div>
 </section>
 
@@ -385,6 +407,7 @@
 	input, select, button {
 		flex: 1;
 	}
+
 
 	.main-content {
 		flex: 1; /* The other two columns will equally share the available space */
